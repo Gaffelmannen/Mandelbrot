2bf9ec8b5f7ebf7746905b3c0c1aebe2f0682eca
Hans Sandström <hans.sandstrom@gmail.com>
2012-06-15 11:59:12 +0200

    Added test methods.

---
+++ Mandelbrot/src/nu/gaffelmannen/apps/mandelbrot/test/TestComplex.java
@@ -0,0 +1,33 @@
+package nu.gaffelmannen.apps.mandelbrot.test;
+
+import static org.junit.Assert.*;
+
+import nu.gaffelmannen.apps.mandelbrot.Complex;
+
+import org.junit.Test;
+
+public class TestComplex {
+
+	@Test
+	public void test_Create_ComplexNumber() {
+		
+		double delta = 0.0d;
+		double a = 3.0d;
+		double b = 4.0d;
+		
+		Complex c = new Complex(a,b);
+		assertEquals(a, c.a, delta);
+		assertEquals(b, c.b, delta);
+	}
+	
+	@Test
+	public void test_absolute_of_ComplexNumber(){
+		
+		double delta = 0.0d;
+		double a = 3.0d;
+		double b = 4.0d;
+		
+		Complex c = new Complex(a,b);
+		assertEquals(5.0d, c.abs(), delta);
+	}
+}
---
+++ Mandelbrot/src/nu/gaffelmannen/apps/mandelbrot/test/TestMandelbrot.java
@@ -0,0 +1,14 @@
+package nu.gaffelmannen.apps.mandelbrot.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestMandelbrot {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
